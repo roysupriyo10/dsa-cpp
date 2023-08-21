c65ba50acf7084fecc39ffc1dad7cfbda0ff6dc7
Supriyo Roy <roysupriyo10@gmail.com>
2023-08-21 06:30:31 +0530

    added ignore for compiled file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <string>
 
 int multiply (int first, int second) {
   return first * second;
 }
 
 int main () {
-  std::string first_number;
-  std::cout << "Please type in your first name : " << std::endl;
-  std::cin >> first_number;
-  std::cout << "Hello" << first_number << std::endl;
-  std::cout << multiply(15, 10) << std::endl;
+  // std::string first_number;
+  // std::cout << "Please type in your first name : " << std::endl;
+  // std::cin >> first_number;
+  // std::cout << "Hello" << first_number << std::endl;
+  // std::cout << multiply(15, 10) << std::endl;
+  int age;
+  std::string name;
+
+  std::cout << "Please enter your age and name : " << std::endl;
+  std::cin >> age;
+  std::cin >> name;
+  std::cout << "Hello " << name << ", you are " << age << " years old!" << std::endl;
   return 0;
 }
