cd6c8ad000631918cf6afb8b21c82bf1e9ddd260
Supriyo Roy <roysupriyo10@gmail.com>
2023-08-21 06:20:46 +0530

    latest

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+int multiply (int first, int second) {
+  return first * second;
+}
+
 int main () {
-  std::cout << "Hello World! This is nice!" << std::endl;
+  std::string first_number;
+  std::cout << "Please type in your first name : " << std::endl;
+  std::cin >> first_number;
+  std::cout << "Hello" << first_number << std::endl;
+  std::cout << multiply(15, 10) << std::endl;
   return 0;
 }
